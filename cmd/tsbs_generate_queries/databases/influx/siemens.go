9a4c1a95a718e7a6dcf22e1d1020144bd0d5254e
Aleksei <as5423.ru@gmail.com>
2019-11-26 13:02:50 +0300

    Add InfluxDB siemens queries

--- cmd/tsbs_generate_queries/databases/influx/siemens.go
+++ cmd/tsbs_generate_queries/databases/influx/siemens.go
@@ -1,9 +1,76 @@
 package influx
 
-import "github.com/timescale/tsbs/cmd/tsbs_generate_queries/uses/siemens"
+import (
+	"fmt"
+	"github.com/timescale/tsbs/cmd/tsbs_generate_queries/uses/siemens"
+	"github.com/timescale/tsbs/query"
+	"time"
+)
 
 // Siemens produces TimescaleDB-specific queries for all the siemens query types.
 type Siemens struct {
 	*BaseGenerator
 	*siemens.Core
+}
+
+func (s *Siemens) RawData(qi query.Query, d time.Duration) {
+	interval := s.Interval.MustRandWindow(d)
+
+	influxql := fmt.Sprintf(`SELECT "value" FROM "%s" WHERE time > '%s' AND time <= '%s'`,
+		s.GetRandomSensor(),
+		interval.Start().Format(time.RFC3339),
+		interval.End().Format(time.RFC3339),
+	)
+
+	humanLabel := "Influx all value from sensor"
+	humanDesc := humanLabel
+
+	s.fillInQuery(qi, humanLabel, humanDesc, influxql)
+}
+
+func (s *Siemens) SampledData(qi query.Query, d, gd time.Duration) {
+	interval := s.Interval.MustRandWindow(d)
+
+	influxql := fmt.Sprintf(`SELECT last("value") FROM "%s" WHERE time > '%s' AND time <= '%s' GROUP BY time(%s)`,
+		s.GetRandomSensor(),
+		interval.Start().Format(time.RFC3339),
+		interval.End().Format(time.RFC3339),
+		gd.String(),
+	)
+
+	humanLabel := "Influx sample value"
+	humanDesc := humanLabel
+
+	s.fillInQuery(qi, humanLabel, humanDesc, influxql)
+}
+
+func (s *Siemens) Maximum(qi query.Query, d, gd time.Duration) {
+	interval := s.Interval.MustRandWindow(d)
+
+	influxql := fmt.Sprintf(`SELECT max("value") FROM "%s" WHERE time > '%s' AND time <= '%s' GROUP BY time(%s)`,
+		s.GetRandomSensor(),
+		interval.Start().Format(time.RFC3339),
+		interval.End().Format(time.RFC3339),
+		gd.String(),
+	)
+
+	humanLabel := "Influx maximum value"
+	humanDesc := humanLabel
+
+	s.fillInQuery(qi, humanLabel, humanDesc, influxql)
+}
+
+func (s *Siemens) Diff(qi query.Query, d time.Duration) {
+	interval := s.Interval.MustRandWindow(d)
+
+	influxql := fmt.Sprintf(`SELECT difference("value") FROM "%s" WHERE time > '%s' AND time <= '%s'`,
+		s.GetRandomSensor(),
+		interval.Start().Format(time.RFC3339),
+		interval.End().Format(time.RFC3339),
+	)
+
+	humanLabel := "Influx difference between values"
+	humanDesc := humanLabel
+
+	s.fillInQuery(qi, humanLabel, humanDesc, influxql)
 }
